fa817d15dd3a56cb76bdb2596a62e359cc8001b1
Otavioqwert <otavioml404@gmail.com>
2026-01-13 01:00:22 -0300

    fix(piloto): correct App.tsx CardCircle prop - restore full file integrity
    - Add activeSynergies prop to CardCircle in correct location
    - Keep full file structure intact

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
           <div className="relative py-12">
-            <CardCircle 
-              slots={slots} 
-              onRemove={handleRemoveCard} 
-              onPlace={handlePlaceCard} 
-              selectedCardIndex={selectedInventoryIndex} 
-              onActivate={handleActivateEffect} 
-              globalHours={globalHours}
-              activeSynergies={activeSynergies}
-            />
+            <CardCircle slots={slots} onRemove={handleRemoveCard} onPlace={handlePlaceCard} selectedCardIndex={selectedInventoryIndex} onActivate={handleActivateEffect} globalHours={globalHours} activeSynergies={activeSynergies} />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center pointer-events-none">
